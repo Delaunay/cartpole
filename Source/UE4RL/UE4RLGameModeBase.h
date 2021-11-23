90e6e03badad2a88a7138db5a2e50508c368cf66
Pierre Delaunay <pierre.delaunay.tr@gmail.com>
2021-11-23 17:15:49 -0500

    init

---
+++ Source/UE4RL/UE4RLGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4RLGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4RL_API AUE4RLGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
